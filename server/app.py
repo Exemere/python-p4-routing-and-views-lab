040acac327adfe348d1a237ea5e135c18a787b1f
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-10-16 15:24:23 +0300

    update

--- server/app.py
+++ server/app.py
@@ -3,6 +3,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return'<h1>Python Operations with Flask Routing and Views</h1>'
+@app.route('/print/<string:name>')
+def print_string(name):
+    print (name)
+    return f'{name}'
+    
+@app.route('/count/<int:num>')
+def count(num):
+    return f"Counting from 1 to {num}"
+pass
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    return f"{num1} {operation} {num2}"
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
